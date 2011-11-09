cd70324a7c78061d427d8c73d3b8dbe606f96ccf
John Terzis <jt2514@columbia.edu>
2011-11-09 00:25:23 -0500

    added sleep function

--- part1/mdb-lookup-server-nc-2.c
+++ part1/mdb-lookup-server-nc-2.c
@@ -20,13 +20,14 @@ int main(int argc, char **argv)
 
 char port[6];    //port number can be up to 65xxx digits plut '\0' , so 6 bytes in total
 printf("port number: ");
-
+memset(port,'0',6);
 while(fgets(port,sizeof(port),stdin)!=NULL)  //read port number from stdin. newline char copied and '\0' appended by default
 {
 	
 //might need to remove newline char from port
 
-
+//hitting enter (newline) should prompt user again
+if(port[0]=='\n') {printf("port number: "); continue;}
 
 /*unnecessary since we are using port number from stdin
     if (argc != 2) {
@@ -42,24 +43,27 @@ while(fgets(port,sizeof(port),stdin)!=NULL)  //read port number from stdin. newl
     } else if (pid == 0) {
 	// child process
 	fprintf(stderr, "[pid=%d] ", (int)getpid());
-	fprintf(stderr, "mdb-lookup-server started on port %s\n", port);
+	fprintf(stderr, "mdb-lookup-server started on port %s with child process id %d \n", port,(int)getpid());
 	execl("./mdb-lookup-server-nc.sh", "mdb-lookup-server-nc.sh", 
 		port, (char *)0);
+	
 	die("execl failed");
     } else {
 	// parent process
-	if (waitpid(pid,
-		    NULL, // no status
-		    0) // no options
-		!= pid)
-	    die("waitpid failed");
-
+	if ((pid=waitpid((pid_t)-1, NULL, WNOHANG)) == -1)
+		
+	    die("waitpid failed"); 
+	if(pid==0){sleep(1); printf("port number: "); memset(port,'0',6); continue;} //if no child immediately available restart loop
+	
+	//o/w pid == value and thus one of the child processes has changed status, print them below
+	
 	fprintf(stderr, "[pid=%d] ", (int)pid);
 	fprintf(stderr, "mdb-lookup-server terminated\n");
     }
 
+sleep(1); 
 printf("port number: ");
-
+memset(port,'0',6);   //rest port to blank
 
 }
 
